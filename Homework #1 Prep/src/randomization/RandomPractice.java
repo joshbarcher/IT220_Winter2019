47b374e61465c85603a44a9aa7341cb0b47b3715
Josh Archer <jarcher@greenriver.edu>
2019-01-10 16:03:55 -0800

    Added Javadocs to our custom class. Added the randomBetween() function as a hint for our HW #1.

--- Homework #1 Prep/src/randomization/RandomPractice.java
+++ Homework #1 Prep/src/randomization/RandomPractice.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 public class RandomPractice 
 {
+	private static Random random = new Random();
+	
 	public static void main(String[] args) 
 	{
 		Random myRandom = new Random(100);
@@ -13,5 +15,10 @@ public class RandomPractice
 			int number = myRandom.nextInt(10);
 			System.out.println(number);
 		}
+	}
+	
+	public static int randomBetween(int low, int high)
+	{
+		return random.nextInt(high - low + 1) + low;
 	}
 }
